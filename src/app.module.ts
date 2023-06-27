56c65a742546603714f60b0d55517f144718e8d3
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-27 23:42:01 +0300

    setup mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { FilterationMiddleware } from './common/helper/filteration-middleware';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { MongooseModule, MongooseModuleFactoryOptions } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -13,7 +14,17 @@ import * as Joi from 'joi';
 
       validationSchema: Joi.object({
         PORT: Joi.number().default(3000),
+        DB_URL: Joi.string().required(),
       }),
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          uri: configService.get('DB_URL'),
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        } as MongooseModuleFactoryOptions),
     }),
     UsersModule,
   ],
