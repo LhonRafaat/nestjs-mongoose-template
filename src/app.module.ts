5670c5a6f253209ed64db3e0de40df1fb576cdea
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-27 23:34:19 +0300

    basic configservice setup and use

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,16 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { FilterationMiddleware } from './common/helper/filteration-middleware';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, //we use the global config so it does not required to import in every module we use
+
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+      }),
     }),
     UsersModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // set to true to throw an error if extra fields were sent by client
@@ -11,6 +14,7 @@ async function bootstrap() {
       disableErrorMessages: true, // use this for production if you do not want to send detailed error messages back to client
     }),
   );
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
+  console.log(`Application running at ${await app.getUrl()}`);
 }
 bootstrap();
