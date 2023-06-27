6bd7c258ad727d8acc0265e324cdc534e5f6ae26
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-27 23:51:59 +0300

    added swagger and init it

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,13 @@ async function bootstrap() {
       disableErrorMessages: true, // use this for production if you do not want to send detailed error messages back to client
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Example Api')
+    .setDescription('A documentation from example api')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(configService.get('PORT'));
   console.log(`Application running at ${await app.getUrl()}`);
 }
