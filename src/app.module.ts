981cb647a880364f7906d4d66eb00a798e444295
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-27 23:16:57 +0300

    installed nest config and added basic configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { FilterationMiddleware } from './common/helper/filteration-middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //we use the global config so it does not required to import in every module we use
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
