483308f4a033889be06c1c5aea8e2a07111a2a12
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-28 19:57:24 +0300

    added jwt sign when loging in

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,20 +2,31 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
 import { LoginPayload } from './dto/login.payload';
+import { RegisterPayload } from './dto/register.payload';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(payload: LoginPayload): Promise<any> {
     const user = await this.usersService.findByEmail(payload.email);
     if (user) {
       const isMatch = await bcrypt.compare(payload.password, user.password);
-
-      if (isMatch) return user;
+      if (isMatch) {
+        const payload = { id: user._id, fullName: user.fullName };
+        return { token: this.jwtService.sign(payload) };
+      }
     }
     throw new UnauthorizedException(
       'Could not authenticate. Please try again.',
     );
+  }
+
+  async register(payload: RegisterPayload) {
+    return await this.usersService.create(payload);
   }
 }
