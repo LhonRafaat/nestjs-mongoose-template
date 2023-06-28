326adaeb3b2b3d8379c05090e844715038e97fe5
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-28 19:56:44 +0300

    added controller route

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,16 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginPayload } from './dto/login.payload';
 import { AuthService } from './auth.service';
+import { RegisterPayload } from './dto/register.payload';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-Controller('auth');
-
+@Controller('auth')
+@ApiTags('Auth')
+@ApiBearerAuth()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   async login(@Body() payload: LoginPayload) {
     return await this.authService.validateUser(payload);
   }
 
   @Post('register')
-  register() {}
+  async register(@Body() payload: RegisterPayload) {
+    return await this.authService.register(payload);
+  }
 }
