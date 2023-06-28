e68f7de23f838042140f411bd7528eca10af3005
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-28 19:58:45 +0300

    added register service and refactored get by email service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,6 +3,8 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { TUser } from './user.model';
+import { RegisterPayload } from '../auth/dto/register.payload';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,6 +14,15 @@ export class UsersService {
     return `This action returns all users`;
   }
 
+  async create(payload: RegisterPayload) {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(payload.password, saltOrRounds);
+    return await this.userModel.create({
+      ...payload,
+      password: hashedPassword,
+    });
+  }
+
   async findOne(id: string): Promise<TUser> {
     const user = await this.userModel.findById(id);
     if (!user) throw new BadRequestException('User not found');
@@ -19,7 +30,9 @@ export class UsersService {
   }
 
   async findByEmail(email: string): Promise<TUser> {
-    return await this.userModel.findOne({ email });
+    const user = await this.userModel.findOne({ email }).select('+password');
+    if (!user) throw new BadRequestException('User not found');
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
