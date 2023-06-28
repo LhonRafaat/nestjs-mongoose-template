8c3e00160a967957c5c05fa38d92ba8e1a923508
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-28 17:54:31 +0300

    added user type

--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -1,1 +1,24 @@
-export class TUser {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class TUser {
+  @ApiProperty()
+  _id: string;
+
+  @ApiProperty()
+  fullName: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  avatar: string;
+
+  @ApiProperty()
+  createdAt: string;
+
+  @ApiProperty()
+  updatedAt: string;
+}
