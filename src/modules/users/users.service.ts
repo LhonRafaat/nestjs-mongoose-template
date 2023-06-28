0e39564c26e8d664a869e2ed2192106ec85bbb6c
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-28 18:18:05 +0300

    added findone service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -12,8 +12,10 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string): Promise<TUser> {
+    const user = await this.userModel.findById(id);
+    if (!user) throw new BadRequestException('User not found');
+    return user;
   }
 
   async findByEmail(email: string): Promise<TUser> {
