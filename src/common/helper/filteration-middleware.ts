75617d81bceba24d26b9d4d29d0d03c1210b25df
lhonrafaat <lhontaekwondo@gmail.com>
2023-06-29 14:46:40 +0300

    refactored filteration middleware to accept multiple search queries

--- src/common/helper/filteration-middleware.ts
+++ src/common/helper/filteration-middleware.ts
@@ -1,27 +1,53 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 import { Response, NextFunction } from 'express';
 import { IRequest } from './common-types';
+import { isArray } from 'class-validator';
 
 @Injectable()
 export class FilterationMiddleware implements NestMiddleware {
   async use(req: IRequest, res: Response, next: NextFunction) {
     let searchObj;
 
-    searchObj = {
-      [`${req.query.search}`]: {
-        $regex: req.query.searchVal,
-        $options: 'i',
-      },
-    };
-    req.query.searchVal !== '' && Number.isInteger(+req.query.searchVal)
-      ? (searchObj = { [`${req.query.search}`]: +req.query.searchVal })
-      : (searchObj = {
-          [`${req.query.search}`]: {
-            $regex: req.query.searchVal,
-            $options: 'i',
-          },
-        });
-
+    if (isArray(req.query.search) && isArray(req.query.searchVal)) {
+      if (req.query.search?.length !== req.query.searchVal?.length)
+        throw new BadRequestException(
+          'Please make sure your search queries match',
+        );
+      req.query.search.forEach((el, index) => {
+        if (
+          req.query.searchVal[index] !== '' &&
+          Number.isInteger(+req.query.searchVal[index])
+        ) {
+          searchObj = {
+            ...searchObj,
+            [`${el}`]: +req.query.searchVal[index],
+          };
+        } else {
+          if (el)
+            searchObj = {
+              ...searchObj,
+              [`${el}`]: {
+                $regex: req.query.searchVal[index],
+                $options: 'i',
+              },
+            };
+        }
+      });
+    } else {
+      req.query.searchVal !== '' && Number.isInteger(+req.query.searchVal)
+        ? (searchObj = { [`${req.query.search}`]: +req.query.searchVal })
+        : req.query.search &&
+          (searchObj = {
+            [`${req.query.search}`]: {
+              $regex: req.query.searchVal,
+              $options: 'i',
+            },
+          });
+    }
     if (!req.query.limit) {
       req.query.limit = '10';
     }
@@ -56,7 +82,6 @@ export class FilterationMiddleware implements NestMiddleware {
       }
     }
     req.dateQr = dateQr;
-    console.log(dateQr);
     req.searchObj = searchObj;
 
     const skip: number = (+req.query.page - 1) * +req.query.limit;
