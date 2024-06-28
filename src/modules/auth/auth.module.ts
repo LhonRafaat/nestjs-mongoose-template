ce2cde5b7414461a6998e40e3f59c02c4a0de901
lhonRafaat <lhontaekwondo@gmail.com>
2024-06-28 21:09:58 +0300

    removed default Jwt mdoule register

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -7,35 +7,11 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthController } from './auth.controller';
 import { EnvConfig } from '../../config.type';
+import { RefreshStrategy } from './strategies/refresh.strategy';
 
 @Module({
-  imports: [
-    UsersModule,
-    PassportModule,
-    JwtModule.registerAsync({
-      useFactory: async (configService: ConfigService<EnvConfig>) => {
-        return {
-          secret: configService.get('ACCESS_SECRET'),
-          signOptions: {
-            expiresIn: configService.get('ACCESS_TOKEN_EXPIRATION'),
-          },
-        };
-      },
-      inject: [ConfigService],
-    }),
-    JwtModule.registerAsync({
-      useFactory: async (configService: ConfigService<EnvConfig>) => {
-        return {
-          secret: configService.get('REFRESH_SECRET'),
-          signOptions: {
-            expiresIn: configService.get('REFRESH_TOKEN_EXPIRATION'),
-          },
-        };
-      },
-      inject: [ConfigService],
-    }),
-  ],
-  providers: [AuthService, JwtStrategy],
+  imports: [UsersModule, PassportModule, JwtModule.register({})],
+  providers: [AuthService, JwtStrategy, RefreshStrategy],
   controllers: [AuthController],
   exports: [PassportModule],
 })
