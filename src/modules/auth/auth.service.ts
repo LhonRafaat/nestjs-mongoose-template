021122914c5d9f7a0ca032da794e7539ef204a69
lhonRafaat <lhontaekwondo@gmail.com>
2024-06-28 20:11:38 +0300

    added get tokens service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,12 +4,15 @@ import * as bcrypt from 'bcrypt';
 import { LoginPayload } from './dto/login.payload';
 import { RegisterPayload } from './dto/register.payload';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { EnvConfig } from '../../config.type';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
+    private readonly configService: ConfigService<EnvConfig>,
   ) {}
 
   async validateUser(payload: LoginPayload): Promise<any> {
@@ -17,8 +20,11 @@ export class AuthService {
     if (user) {
       const isMatch = await bcrypt.compare(payload.password, user.password);
       if (isMatch) {
-        const payload = { id: user._id, fullName: user.fullName };
-        return { token: this.jwtService.sign(payload) };
+        const tokens = await this.getTokens({
+          _id: user._id,
+          fullName: user.fullName,
+        });
+        return { tokens };
       }
     }
     throw new UnauthorizedException(
@@ -28,5 +34,27 @@ export class AuthService {
 
   async register(payload: RegisterPayload) {
     return await this.usersService.create(payload);
+  }
+
+  async getTokens(payload: { _id: string; fullName: string }) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get('ACCESS_SECRET'),
+        expiresIn: this.configService.get('ACCESS_TOKEN_EXPIRATION'),
+      }),
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get('REFRESH_SECRET'),
+        expiresIn: this.configService.get('REFRESH_TOKEN_EXPIRATION'),
+      }),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async hashData(data: string) {
+    return await bcrypt.hash(data, 10);
   }
 }
