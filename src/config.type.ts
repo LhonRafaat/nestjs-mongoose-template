aa36a0062ccb30e918363f6d2803ea5dd6863c98
lhonRafaat <lhontaekwondo@gmail.com>
2024-06-28 19:51:25 +0300

    added config type

---
+++ src/config.type.ts
@@ -0,0 +1,8 @@
+export type EnvConfig = {
+  PORT: string;
+  DB_URL: string;
+  ACCESS_SECRET: string;
+  REFRESH_SECRET: string;
+  ACCESS_TOKEN_EXPIRATION: string;
+  REFRESH_TOKEN_EXPIRATION: string;
+};
