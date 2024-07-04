9f7d22b675ce008c45b99f099dcc92651be4ae99
lhonRafaat <lhontaekwondo@gmail.com>
2024-07-04 21:19:55 +0300

    added user stub

---
+++ src/modules/users/test/user.stub.ts
@@ -0,0 +1,15 @@
+import { TUser } from '../user.model';
+
+export const userStub = (): TUser => {
+  return {
+    _id: '667ef71fb67a3ddc02f2adc3',
+    fullName: 'lhon',
+    email: 'example@gmail.com',
+    refreshToken: '132324',
+    password: '12345',
+    avatar: 'slaw',
+    isAdmin: false,
+    createdAt: '2024-06-28T17:47:11.539Z',
+    updatedAt: '2024-07-04T12:49:02.693Z',
+  };
+};
