b8d0673ebf6f72c9313bc9a11ccc45f300990f2c
lhonRafaat <lhontaekwondo@gmail.com>
2024-10-11 19:50:37 +0300

    feat: add google env variables

--- src/config.type.ts
+++ src/config.type.ts
@@ -5,4 +5,7 @@ export type EnvConfig = {
   REFRESH_SECRET: string;
   ACCESS_TOKEN_EXPIRATION: string;
   REFRESH_TOKEN_EXPIRATION: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  GOOGLE_CALLBACK_URL: string;
 };
